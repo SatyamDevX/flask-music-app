9d078c0f2b242d9a95ebfa88ab745e9c47025af8
SatyamDevX <satyam.development@gmail.com>
2025-05-06 23:56:45 +0530

    song ulpload feature is added song file is added in  static/upload folder and other song attributes added in database

---
+++ music/song.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, render_template, request, current_app
+
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
+
+from flask_security import current_user
+
+import os
+
+
+from music.models import db, Song
+
+
+song_bp = Blueprint('song', __name__, url_prefix='/song')
+
+
+@song_bp.route('/upload', methods=('GET','POST'))
+def song_upload():
+   if request.method == 'POST':
+      title = request.form['title']
+      artist = request.form['artist']
+      file = request.files['file']
+      filename = file.filename
+
+      #Enuser upload folder extist
+      os.makedirs(current_app.config['UPLOAD_FOLDER'], exist_ok=True)
+      file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+      
+      # Save song info in DB
+      song = Song(
+            title=title,
+            artist=artist,
+            file_path=filename,  # Only filename; path handled by Flask
+            creator_id=current_user.id  # assumes user is logged in
+      )
+      db.session.add(song)
+      db.session.commit()
+
+
+      print(title, artist, filename)
+   return render_template('song_upload.html')
+
+
