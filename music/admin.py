48258e076efd3434f9c6e6c882d7c407e7ed10e4
SatyamDevX <satyam.development@gmail.com>
2025-06-02 15:38:09 +0530

    showing approved creator requested and add block route to block them

--- music/admin.py
+++ music/admin.py
@@ -25,8 +25,10 @@ def admind_ashboard():
 @roles_required('admin')  # Flask-Security decorator
 def approve_creators():
     pending_users = User.query.filter_by(creator_requested=True, is_creator=False).all()
-    return render_template('approve_creators.html', users=pending_users)
+    approved_users = User.query.filter_by(is_creator=True).all()
+    return render_template('approve_creators.html', users=pending_users, creators=approved_users)
 
+@admin_bp.route('/block_creator/<int:user_id>', methods=['POST'])
 @admin_bp.route('/approve_creator/<int:user_id>', methods=['POST'])
 @roles_required('admin')
 def approve_creator_action(user_id):
@@ -37,12 +39,18 @@ def approve_creator_action(user_id):
         creator_role = Role(name='creator', description='Creator role')
         db.session.add(creator_role)
         db.session.commit()
+    if user.is_creator:
+        user.roles.remove(creator_role)
+        user.is_creator = False
+        db.session.commit()
+        flash(f"{user.username} has been Blocked as a creator.", 'success')
+        return redirect(url_for('admin.approve_creators'))
+    else:
+        user.roles.append(creator_role)
+        user.is_creator = True
+        user.creator_requested = False
+        db.session.commit()
 
-    user.roles.append(creator_role)
-    user.is_creator = True
-    user.creator_requested = False
-    db.session.commit()
-
-    flash(f"{user.username} has been approved as a creator.", 'success')
-    return redirect(url_for('admin.approve_creators'))
+        flash(f"{user.username} has been approved as a creator.", 'success')
+        return redirect(url_for('admin.approve_creators'))
 
